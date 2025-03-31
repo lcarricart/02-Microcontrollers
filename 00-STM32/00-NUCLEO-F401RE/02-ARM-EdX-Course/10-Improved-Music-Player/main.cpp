533b27af23c38347892101aac670606cc3d92fcf
Luciano Carricart <wzv135@haw-hamburg.de>
2025-04-01 00:35:22 +0200

    Minor adjustments to ARM-EdX-Course, Final Project

--- 00-STM32/00-NUCLEO-F401RE/02-ARM-EdX-Course/10-Improved-Music-Player/main.cpp
+++ 00-STM32/00-NUCLEO-F401RE/02-ARM-EdX-Course/10-Improved-Music-Player/main.cpp
@@ -1,11 +1,12 @@
 /*******************************************************************************************************************
- * Objective of the program: Using "Flags-Based-Thread-Control" project as the base, I modified the code to select 
- songs through an UP / DOWN / OK mechanism, giving the user the posibility to browse through songs instead of having 
- to press complex button combinations. The program works underl flags-based thread synchronization.
+ * Objective of the program: Using Project 09 as the base, the selection of songs is improved by implementing an 
+ UP / DOWN / OK mechanism, giving the user the posibility to browse through the menu instead of having to press 
+ complex button combinations. The program works under flags-based thread synchronization (use of flag variables at the 
+ end of each thread).
  *******************************************************************************************************************
- Learning outcome: I realize that RTOS programming isn't just like any other program flow. There are different
- strategies to control the flow safely, and the one imnplemented here is flag-based (or state-variable) thread
- synchronization (use of flag variables at the end of each thread).
+ Learning outcome: RTOS´s program flows are radically different compared to conventional programming code structures. 
+ There are different strategies to control the flow safely, and the one imnplemented here is flag-based (or 
+ state-variable) thread synchronization.
  *******************************************************************************************************************
  * Author: Luciano Carricart, https://github.com/lcarricart/
  * Status: Information Engineering student at HAW Hamburg, Germany.
@@ -31,7 +32,7 @@ Mutex lcd_mutex;
 // These flags indicate state of player
 bool welcome = 1;   // Indicates welcome state
 bool waiting = 0;   // waiting for tune selection
-bool triggered = 0;    // tune is selected but hasn't started yet
+bool triggered = 0; // tune is selected but hasn't started yet
 bool playing = 0;   // tune is playing
 
 // Song selection mechanism
@@ -48,12 +49,15 @@ void Play_tune(void);
 
 // Displays welcome message on LCD
 Thread thread1;
-void LCD_cont() {
-    init_lcd();  // initialise the LCD
+void LCD_cont() 
+{
+    init_lcd();
 
     // Waiting for song
-    while (1) {
-        if (welcome) {
+    while (1) 
+    {
+        if (welcome) 
+        {
             lcd_mutex.lock();
             clr_lcd();             // Clear the LCD
             print_lcd("Your MUSIC Player!");
@@ -64,8 +68,8 @@ void LCD_cont() {
             lcd_mutex.unlock();
             thread_sleep_for(500);
             welcome = 0;
-        } // end of if
-    } // end of while(1)
+        }
+    }
 }
 
 // Displays the Song-selection menu
@@ -101,17 +105,21 @@ void Tune_menu()
             lcd_mutex.unlock();
             thread_sleep_for(100);
 
-            next_menu = 0;  // Turn off song selection menu variable to avoid infinite displays of the menu.
+            next_menu = 0;  // Turn off song selection menu variable to avoid infinite displays of the menu (flags flow control)
         }
     }
 }
 
 // Reads the select buttons and prepares the song
 Thread thread2;
-void Tune_select() {
-    while (1) {
-        if (ok_button) { // "triggered" is set by Interrupt
-            switch (cursor) { // read song selection and load song pointer
+void Tune_select() 
+{
+    while (1) 
+    {
+        if (ok_button)           // "ok_button" is set by Interrupt
+        {        
+            switch (cursor)      // read song selection and load song pointer
+            {    
             case 1: song_ptr = &Oranges; break;
             case 2: song_ptr = &Cielito; break;
             case 3: song_ptr = &Malaika; break;
@@ -121,10 +129,10 @@ void Tune_select() {
             case 7: song_ptr = &Matilda; break;
             case 8: song_ptr = &Alouetta; break;
             case 9: song_ptr = &Twinkle; break;
-            } // end of switch
+            }
 
             lcd_mutex.lock();
-            clr_lcd();             // Clear the LCD
+            clr_lcd();           
             print_lcd("Now playing:");
             write_cmd(0xc0);
             print_lcd(song_ptr->name); // display song name
@@ -134,25 +142,28 @@ void Tune_select() {
 
             ok_button = 0;
             playing = 1;
-        } // end of if
-    } // end of while(1)
+        }
+    }
 }
 
 // Plays the chosen tune
 Thread thread3;
-void Play_tune() {
-    while (1) {
-        if (playing) {
-            // resume PWM operation, stopped after last song
+void Play_tune() 
+{
+    while (1) 
+    {
+        if (playing) 
+        {
+            // Resume PWM operation, stopped after last song
             speaker.resume();
             for (int i = 0; i <= (song_ptr->length); i++) {
-                speaker.period(1 / (2 * (*song_ptr).freq[i])); // set PWM period
-                speaker = volume; // set duty cycle, hence volume control
-                thread_sleep_for(200 * (*song_ptr).beat[i]); // hold for beat period
+                speaker.period(1 / (2 * (*song_ptr).freq[i]));  // set PWM period
+                speaker = volume;                               // set duty cycle, hence volume control
+                thread_sleep_for(200 * (*song_ptr).beat[i]);    // hold for beat period
             }
-            speaker = 0; // To shut the sound, volume = 0;
+            speaker = 0;                                        // To shut the sound, volume = 0;
 
-            // indicate end of song
+            // Indicate end of song
             lcd_mutex.lock();
             write_cmd(0xD4);
             print_lcd("Status: Waiting...");
@@ -162,15 +173,15 @@ void Play_tune() {
             playing = false;
             welcome = 1;
             speaker.suspend();
-        } // end of if
-    } // end of while(1)
+        }
+    }
 }
 
 /*-------------- Handlers ---------------*/
 
 // Responds to press of GO button, and sets "next_menu"
 void go_handler() {
-    if (playing == 0) // only set "triggered" if song not playing
+    if (playing == 0) // Only set "next_menu" if the song is not playing
         next_menu = 1;
 }
 
@@ -203,8 +214,8 @@ void ok_handler()
 }
 
 //--------------- Main ------------------//
-int main() {
-
+int main() 
+{
     // Falling edge interrupts for buttons
     go.fall(&go_handler);      
     arrow_down.fall(&down_handler);
@@ -217,7 +228,8 @@ int main() {
     thread2.start(callback(Tune_select));
     thread3.start(callback(Play_tune));
 
-    while (1) {
-        __WFI(); // wait for interrupt
+    while (1) 
+    {
+        __WFI(); // Wait For Interrupt
     }
 }
