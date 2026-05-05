b123e7fa51172ba221a189f0d0a000913b520d65
Luciano Carricart <wzv135@haw-hamburg.de>
2026-05-06 01:13:53 +0200

    Fix keypad LAB exercise. Problem solved by looking at voltage levels in each pin after button pressed

--- 06-Texas-Instruments/01_TM4C1294_keypad/main.c
+++ 06-Texas-Instruments/01_TM4C1294_keypad/main.c
@@ -1,22 +1,27 @@
-/** Default Project for TM4C1294ncpdt
- *
+/*******************************************************************************************
  * File:    main.c
- * Author:
- * Date:
- * Version:
- *
- * Blinks the onboard LED using a busy-wait loop as delay
+ * Target:  TM4C1294ncpdt
+ * Author:  Luciano Carricart
+ *******************************************************************************************
+ * @brief
+ * Recognize the button pressed in a keypad via polling
+ *******************************************************************************************
+ * Learning Outcomes
+ *  - The best debugging was using the multimeter. The LEDs of my setup were increasing the
+      voltage seen in the line (kinda negative drop) and instead of 1.5V I was geting 2.5V.
+      Once removing the LEDs, the line dropped to 1.5V, seen as a digital 0. I dont think
+      I should have voltage at all when pressing but maybe my homemade hardware setup wasn't
+      perfect. In the lab we were overengineering it when a multimeter could have been enough.
+ *******************************************************************************************
  */
 
 #include "inc/tm4c1294ncpdt.h"
 #include <stdint.h>
 #include <stdio.h>
 
-const char symbol[4][4] = {
-        {'1', '2', '3', 'F'},
-        {'4', '5', '6', 'E'},
-        {'7', '8', '9', 'D'},
-        {'A', '0', 'B', 'C'}
+const char symbol[2][3] = {
+        {'A', 'B', 'C'},
+        {'D', 'E', 'F'}
     };
 
 /*******************************************************************************************/
@@ -27,80 +32,48 @@ void wait(int ticks) {
 
 /*******************************************************************************************/
 
-void configure_led() {
-    SYSCTL_RCGCGPIO_R       |= (1 << 9);  // switch on clock for Port K
-    SYSCTL_RCGCGPIO_R       |= (1 << 10);  // switch on clock for Port L
+void configure_pins() {
+    /* Clock the ports */
+    SYSCTL_RCGCGPIO_R       |= (1 << 9);    // Port K
+    SYSCTL_RCGCGPIO_R       |= (1 << 10);   // Port L
     
     /* TODO: set a while loop until the clock ready register is set */
     wait(2000);
 
-    GPIO_PORTK_DEN_R        |= (0x7 << 0);   // digital I/O enable pin PK1
-    GPIO_PORTK_DIR_R        |= 0x07;         // set PortK 1 Output
-    GPIO_PORTK_DATA_R        = 0x00;         // set PortK Output to zero
+    /* Enable, Direction, and Data registers for each port */
+    GPIO_PORTK_DEN_R        |= 0x07;         // digital I/O enable pin PK1
+    GPIO_PORTK_DIR_R        |= 0x07;         // set PortK Output
+    GPIO_PORTK_DATA_R        = 0x00;         // Initialize to zero
 
-    GPIO_PORTL_DEN_R        |= (0x3 << 0);   // digital I/O enable pin PL1
-    GPIO_PORTL_DIR_R        |= 0x00;         // set PortL 1 Input
-    GPIO_PORTL_DATA_R        = 0x00;         // set PortL Output to zero
+    GPIO_PORTL_DEN_R        |= 0x03;         // digital I/O enable pin PL1
+    GPIO_PORTL_DIR_R        |= 0x00;         // set PortL Input
+    GPIO_PORTL_DATA_R        = 0x00;         // Initialize the values
 }
-
-
 
 /*******************************************************************************************/
 
 /*******************************************************************************************/
 
 int main(void) {
-
-    // Configure LED 1
-    configure_led();
+    // Initialize ports and pins
+    configure_pins();
     wait(2000);
 
-    printf("\n...\r\n");
-    printf("Program started\r\n");
+    printf("\nProgram started!\r\n");
     wait(2000);
-
-    GPIO_PORTL_DATA_R = 0x00;
 
     while(1) {
         for(int i = 0; i < 3; i++) {
-            /* Traversing this lines is done correctly */
-            GPIO_PORTK_DATA_R = (0x01 << i);
+            /* High volzage = 1, therefore we need to set 0s one at a time to GND the line */
+            GPIO_PORTK_DATA_R = ~(1 << i); /* This is a bit innacurate because it sets all the bits in the register, not only the used ones */
             wait(2000);
 
             for(int k = 0; k < 2; k++) {
                 if((GPIO_PORTL_DATA_R & (1 << k)) == 0) {
-                    //if(GPIO_PORTK_DATA_R & (1 << i) == 1) {
                     printf("Key pressed: %c \r\n", symbol[k][i]);
                     wait(2000);
-                    //}
                 }
             }
         }
     }
 }
-
-
-// switch (GPIO_PORTK_DATA_R) {
-//     case (1 << 0):
-//         wait(2000);
-//         printf("\nKey pressed: %c", symbol[k][0]);
-//     break;
-
-//     case (1 << 1):
-//         wait(2000);
-//         printf("\nKey pressed: %c", symbol[k][1]);
-//     break;
-
-//     case (1 << 2):
-//         wait(2000);
-//         printf("\nKey pressed: %c", symbol[k][2]);
-//     break;
-
-//     case (1 << 3):
-//         wait(2000);
-//         printf("\nKey pressed: %c", symbol[k][3]);
-//     break;
-
-//     default:
-//     break;
-// }
