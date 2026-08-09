8d30d15ee93a102c25c84b456bdd2e50e060729b
Luciano Carricart <wzv135@haw-hamburg.de>
2026-08-09 02:49:43 +0200

    Minor changes

--- 00-STM32/00-NUCLEO-F401RE/03-Extra-Practice/11_mutex_oled/SSD1306_OLED_Driver/Core/Src/main.c
+++ 00-STM32/00-NUCLEO-F401RE/03-Extra-Practice/11_mutex_oled/SSD1306_OLED_Driver/Core/Src/main.c
@@ -54,11 +54,10 @@ static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
 static void MX_USART2_UART_Init(void);
 void StartDefaultTask(void *argument);
-void OledWriter1_Task(void *pvParameters);
-void OledWriter2_Task(void *pvParameters);
 
 /* USER CODE BEGIN PFP */
-
+void OledWriter1_Task(void *pvParameters);
+void OledWriter2_Task(void *pvParameters);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -375,6 +374,28 @@ void StartDefaultTask(void *argument)
     osDelay(1);
   }
   /* USER CODE END 5 */
+}
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM1 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM1)
+  {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
 }
 
 /**
