1dbf5585c46f4ee8161c5f8f7cf6f0252c174600
Luciano Carricart <wzv135@haw-hamburg.de>
2026-08-12 01:15:19 +0200

    Add comments to start documenting the first steps properly

--- 04-NXP/02-frdm-mcxn947/00_zephyr_simple_app/src/main.c
+++ 04-NXP/02-frdm-mcxn947/00_zephyr_simple_app/src/main.c
@@ -1,7 +1,12 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 
+/* In Zephyr, startup code, architecture init, the kernel, and configured device init run before application main(). The latter executes as the main thread. One does not typically call a global scheduler-start function in this RTOS */
 int main(void)
 {
+    while (1)
+    {
+        k_sleep(K_SECONDS(1));
+    }
     return 0;
 }
