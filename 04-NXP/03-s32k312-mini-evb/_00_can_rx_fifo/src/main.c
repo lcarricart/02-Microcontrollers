d0a33a3eea8ead4731009a4c602573c3f2ed05da
Luciano Carricart <wzv135@haw-hamburg.de>
2026-08-15 23:50:22 +0200

    S32K3 CAN rxFIFO project start

---
+++ 04-NXP/03-s32k312-mini-evb/_00_can_rx_fifo/src/main.c
@@ -0,0 +1,9 @@
+/*------------------------------------------------------------------------------------------------------------------
+ * Objective: CAN Receiver application using the Legacy RxFIFO feature of the FlexCAN driver. The project is part of
+ * a node to node system, and the CAN Sender project can be found in FRDM-MCXN947/ running Zephyr.
+ -------------------------------------------------------------------------------------------------------------------
+ * Conclusions:
+ -------------------------------------------------------------------------------------------------------------------
+ * Author: Luciano Carricart, https://github.com/lcarricart/
+ * Status: Information Engineering student, HAW Hamburg, Germany.
+ ------------------------------------------------------------------------------------------------------------------*/
