c7e5732af7704ceb53098eabfa46e327ec146272
Luciano Carricart <wzv135@haw-hamburg.de>
2026-08-16 00:30:54 +0200

    Attached SDK

--- 04-NXP/03-s32k312-mini-evb/_00_can_rx_fifo/src/main.c
+++ 04-NXP/03-s32k312-mini-evb/_00_can_rx_fifo/src/main.c
@@ -2,8 +2,35 @@
  * Objective: CAN Receiver application using the Legacy RxFIFO feature of the FlexCAN driver. The project is part of
  * a node to node system, and the CAN Sender project can be found in FRDM-MCXN947/ running Zephyr.
  -------------------------------------------------------------------------------------------------------------------
+ * Remarks: I spent a long time configuring the toolchain for this project, and the SDK for this board expects a
+ * project configured for NXP GCC 10.2, extension that isn't default and needs to be installed.
+ -------------------------------------------------------------------------------------------------------------------
  * Conclusions:
  -------------------------------------------------------------------------------------------------------------------
  * Author: Luciano Carricart, https://github.com/lcarricart/
  * Status: Information Engineering student, HAW Hamburg, Germany.
  ------------------------------------------------------------------------------------------------------------------*/
+
+/**
+*   @file main.c
+*
+*   @addtogroup main_module main module documentation
+*   @{
+*/
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#include "Mcal.h"
+
+int main(void)
+{
+
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} */
